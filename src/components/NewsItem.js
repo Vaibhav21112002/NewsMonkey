e7d8c5a697919b323ff643218859c4b273ca5555
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-09-02 12:19:01 +0530

    Project Compled

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, image, url } = this.props;
+    let { title, description, image, url, author, date,source } = this.props;
     return (
       <div>
-        <div
-          className="card my-2"
-          style={{ minHeight: "500px" }}
-        >
+        <div className="card my-2" style={{ minHeight: "500px" }}>
+          <span style = {{zIndex:"999", left:"90%"}} class="position-absolute top-0 translate-middle badge rounded-pill bg-danger">
+            {source}
+          </span>
           <img src={image} className="card-img-top" alt="..." />
           <div
             className="card-body h-full"
@@ -16,6 +16,12 @@ export class NewsItem extends Component {
           >
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "Unknown"} on{" "}
+                {date ? date.slice(0, 10) : ""}
+              </small>
+            </p>
             <a
               href={url}
               className="btn btn-primary"
